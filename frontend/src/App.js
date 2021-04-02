b32ec07231cb9919028b594ea866721df605b470
Deepakkumar15 <dek350789.dk@gmail.com>
2021-04-02 23:13:53 +0530

    2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react' ;
 import './App.css';
-import Login from './authentication/Login' ;
-import Sign_Up from  './authentication/Sign-Up';
+import Home from './Home';
 
 function App() {
   return (
       <React.Fragment>
-        <Sign_Up />
+        <Home />
       </React.Fragment>
   );
 }
---
+++ frontend/src/Home.js
@@ -0,0 +1,13 @@
+import React from 'react' ;
+// import {  } from 
+import './Home.css' ;
+
+const Home = () => {
+    return(
+        <div>
+            HOME PAGE FOR BOOK MY TICKET
+        </div>
+    ) ;
+};
+
+export default Home;
